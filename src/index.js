6240756c4c3c1c26487434aa435ed6866b5361da
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-10-21 13:47:01 -0500

    added msg

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send({ msg: 'Hello There' })
+  res.send({ msg: 'Hello there! My name is Alonso Hernandez.' })
 })
 
 app.listen(port)
