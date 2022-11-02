d4cdb06b5957590c76f86e7936503b984e34dc03
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-11-01 19:58:48 -0500

    added basic model

--- src/index.js
+++ src/index.js
@@ -4,18 +4,20 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
+import bodyParser from 'body-parser'
 
 import logger from './helpers/logger'
 import router from './routes'
 import { notFound, errorHandler } from './helpers/errors'
 
-const port = parseInt(process.env.PORT, 10) || 3000
+const port = Number(process.env.PORT)
 
 const app = express()
 
 app.use(morgan(process.env.MORGAN_LOG))
 app.use(cors({ origin: process.env.ORIGIN }))
 app.use(helmet())
+app.use(bodyParser.json())
 
 app.use(router)
 
--- src/routes/api/persons.js
+++ src/routes/api/persons.js
@@ -1,26 +1,38 @@
 import { Router } from 'express'
-import { reset } from 'nodemon'
+
+import {
+  getPersons,
+  getPerson,
+  createPerson,
+  updatePerson,
+  deletePerson,
+} from '../../models/persons'
 
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.send({ msg: 'Get all Persons' })
+  const persons = getPersons()
+  res.send(persons)
 })
 
 router.get('/:id', (req, res) => {
-  res.send({ msg: `Getting Person ${req.params.id}` })
+  const person = getPerson(req.params.id)
+  res.send(person)
 })
 
 router.post('/', (req, res) => {
-  res.send({ msg: 'Creating a new Person' })
+  const newPerson = createPerson(req.body)
+  res.send(newPerson)
 })
 
 router.put('/:id', (req, res) => {
-  res.send({ msg: `Updating Person ${req.params.id}` })
+  updatePerson(req.params.id, req.body)
+  res.send({ msg: `Person ${req.params.id} updated.` })
 })
 
 router.delete('/:id', (req, res) => {
-  res.send({ msg: `Deleting Person ${req.params.id}` })
+  deletePerson(req.params.id)
+  res.send({ msg: `Person ${req.params.id} deleted.` })
 })
 
 export default router
