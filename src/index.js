065608817236db0083d875a45ca2410b0fc1a145
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-11-25 10:19:07 -0600

    moved auth

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
-import basicAuth from 'express-basic-auth'
 
 import logger from './helpers/logger'
 import router from './routes'
@@ -15,15 +14,10 @@ const port = Number(process.env.PORT)
 
 const app = express()
 
-app.use(
-  basicAuth({
-    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
-  }),
-)
 app.use(morgan(process.env.MORGAN_LOG))
 app.use(
   cors({
-    origin: process.env.ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     exposedHeaders: ['x-total-count', 'x-total-pages'],
   }),
 )
